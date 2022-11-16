f06750c6188044c07fcab45a437e54a38b7070c2
unknown <benbushman98@gmail.com>
2022-11-15 21:07:28 -0700

    added additional pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import Home from './pages/Home'
+import Home from './components/pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
