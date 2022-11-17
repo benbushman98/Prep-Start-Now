e951f646c292e1fec5d1b7c95f01110a407a5c11
unknown <benbushman98@gmail.com>
2022-11-17 07:52:09 -0700

    setting up header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  Flex,
+  HStack,
+  IconButton,
+  useBreakpointValue,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { FiMenu } from 'react-icons/fi'
+// import Logo from './logo.png'
 
 const Header = () => {
+  const isDesktop = useBreakpointValue({ base: false, lg: true })
   return (
-    <div>Header</div>
+    <Box as="section" pb={{ base: '12', md: '24' }}>
+      <Box as="nav" bg="bg-surface" boxShadow={useColorModeValue('sm', 'sm-dark')}>
+        <Container py={{ base: '4', lg: '5' }}>
+          <HStack spacing="10" justify="space-between">
+            
+            {isDesktop ? (
+              <Flex justify="space-between" flex="1">
+                <ButtonGroup variant="link" spacing="8">
+                  {['Product', 'Pricing', 'Resources', 'Support'].map((item) => (
+                    <Button key={item}>{item}</Button>
+                  ))}
+                </ButtonGroup>
+                <HStack spacing="3">
+                  <Button variant="ghost">Sign in</Button>
+                  <Button variant="primary">Sign up</Button>
+                </HStack>
+              </Flex>
+            ) : (
+              <IconButton
+                variant="ghost"
+                icon={<FiMenu fontSize="1.25rem" />}
+                aria-label="Open Menu"
+              />
+            )}
+          </HStack>
+        </Container>
+      </Box>
+    </Box>
   )
 }
-
-export default Header
+export default Header;
