ef407b970fbaa862ff954f48ded182ddd6ce72dc
unknown <benbushman98@gmail.com>
2022-11-17 23:33:30 -0700

    Added responsiveness

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,6 @@
-import { Image, Flex, Button, HStack, Center, ButtonGroup, chakra, Wrap, WrapItem } from '@chakra-ui/react';
+import {
+  //  Image,
+   Flex, Button, HStack, ButtonGroup, chakra, Wrap, WrapItem } from '@chakra-ui/react';
 // import Logo from '../public/logo.svg';
 // import { Link } from 'react-scroll'
 import React from "react";
@@ -11,14 +13,14 @@ const Header = () => {
         px="6"
         py="5"
         align="center"
-        justify="space-between"
+        justify="center"
       >
-        <Center w="100%">
+        {/* <Center w="100%"> */}
           {/* // Logo */}
           {/* <Image src={Logo.src} h="50px" /> */}
           {/* // Nav Items */}
-          <HStack spacing="8" justify="center" display={{ base: "none", md: "flex" }}>
-            <Flex justify="center" flex="1">
+          <HStack spacing="8" display={{ base: "none", md: "flex" }}>
+            <Flex flex="1">
 
               <ButtonGroup variant="link" spacing="10">
                 
@@ -26,7 +28,7 @@ const Header = () => {
 
                     {['Featured Brands', 'Electronics', 'Food Storage/72-hour Kits', 'Household Essentials', 'Auto Industrial', 'Promotions', 'Health & Safety', 'Resource Center'].map((item) => (
                       <WrapItem>
-                        <Button spacing="8" m={6} key={item}>{item}</Button>
+                        <Button spacing="4" m={4} key={item}>{item}</Button>
                       </WrapItem>
                     ))}
 
@@ -37,10 +39,10 @@ const Header = () => {
             </Flex>
           </HStack>
           {/* // Call to action items */}
-          <HStack >
-            <MobileDrawer display={{ base: "none", md: "flex" }} />
+          <HStack justify="right" >
+            <MobileDrawer display={{ base: "none", md: "flex" }} justify="right"/>
           </HStack>
-        </Center>
+        {/* </Center> */}
       </Flex>
 
     </chakra.header>
